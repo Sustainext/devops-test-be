f3b5fbc33d3aea028745072ce2a38536391cd159
Yashwanth39 <yaswanth.vanimina@sustainext.ai>
2024-05-31 16:09:21 +0530

    changing the load_data file and the mistakes in images

--- sustainapp/management/commands/load_data.py
+++ sustainapp/management/commands/load_data.py
@@ -1,185 +1,221 @@
 from django.core.management.base import BaseCommand
 from sustainapp.models import Framework, Sdg, Certification, Regulation, Rating, Target
-
-
+ 
+ 
 class Command(BaseCommand):
     # help = 'Loads initial data into the database'
-
+ 
     def handle(self, *args, **kwargs):
-
+ 
         models_to_check = [Certification, Framework, Rating, Regulation, Sdg, Target]
-        models_with_existing_data = []
+ 
         for model in models_to_check:
             if model.objects.exists():
                 self.stdout.write(
                     self.style.SUCCESS(f"Data already exists for model {model}")
                 )
-                models_with_existing_data.append(model)
-
-        if models_with_existing_data:
-            delete_choice = input(
-                "Data already exists for the above models. Do you want to delete existing data and add? (yes/no): "
+ 
+        self.update_or_create_certifications()
+        self.update_or_create_frameworks()
+        self.update_or_create_ratings()
+        self.update_or_create_regulations()
+        self.update_or_create_sdgs()
+        self.update_or_create_targets()
+ 
+        self.stdout.write(self.style.SUCCESS("Data Updated successfully"))
+ 
+    def update_or_create_certifications(self):
+        certifications = [
+            {"id": 1, "name": "BCORP", "Image": "images/certifications/BCorp.png"},
+            {
+                "id": 2,
+                "name": "ISO 14001",
+                "Image": "images/certifications/ISO_14001.png",
+            },
+            {"id": 3, "name": "UNGC", "Image": "images/certifications/UNGC.png"},
+        ]
+        for cert in certifications:
+            Certification.objects.update_or_create(id=cert["id"], defaults=cert)
+ 
+    def update_or_create_frameworks(self):
+        frameworks = [
+            {
+                "id": 1,
+                "name": "GRI: With reference to",
+                "Image": "images/framework/GRI.png",
+            },
+            {
+                "id": 2,
+                "name": "GRI: In accordance with",
+                "Image": "images/framework/GRI.png",
+            },
+            {"id": 3, "name": "CDP", "Image": "images/framework/CDP.png"},
+            {"id": 4, "name": "BRSR", "Image": "images/framework/SEBI.png"},
+            {"id": 5, "name": "SASB", "Image": "images/framework/SASB.png"},
+            {"id": 6, "name": "TCFD", "Image": "images/framework/TCFD.png"},
+            {"id": 7, "name": "UNPRI", "Image": "images/framework/UNPRI.png"},
+        ]
+        for framework in frameworks:
+            Framework.objects.update_or_create(id=framework["id"], defaults=framework)
+ 
+    def update_or_create_ratings(self):
+        ratings = [
+            {"id": 1, "name": "DJSI", "Image": "images/rating/DJSI.png"},
+            {"id": 2, "name": "MSCI", "Image": "images/rating/MSCI.png"},
+            {
+                "id": 3,
+                "name": "SUSTAINALYTICS",
+                "Image": "images/rating/SUSTAINALYTICS.png",
+            },
+        ]
+        for rating in ratings:
+            Rating.objects.update_or_create(id=rating["id"], defaults=rating)
+ 
+    def update_or_create_regulations(self):
+        regulations = [
+            {"id": 1, "name": "SB-253", "Image": "images/regulation/SB-253.png"},
+            {"id": 2, "name": "SB-261", "Image": "images/regulation/SB-261.png"},
+            {
+                "id": 3,
+                "name": "BILL S-211",
+                "Image": "images/regulation/Bill_S-211.png",
+            },
+            {"id": 4, "name": "GHGRP", "Image": "images/regulation/GHGRP.png"},
+        ]
+        for regulation in regulations:
+            Regulation.objects.update_or_create(
+                id=regulation["id"], defaults=regulation
             )
-            if delete_choice.lower() == "yes":
-                for model in models_with_existing_data:
-                    model.objects.all().delete()
-            else:
-                self.stdout.write(
-                    self.style.WARNING("Operation aborted by user. Exiting...")
-                )
-                return
-
-        # self.stdout.write(self.style.SUCCESS('Loading the data into Database'))
-
-        Certification.objects.create(
-            name="BCORP", Image="images/certifcations/BCorp_RZCvzp6.png"
-        )
-        Certification.objects.create(
-            name="ISO 14001", Image="images/certifcations/ISO_14001_AyX5yip.png"
-        )
-        Certification.objects.create(
-            name="UNGC", Image="images/certifcations/UNGC_Oe6Ede6.png"
-        )
-
-        Framework.objects.create(
-            name="GRI: With reference to", Image="images/framework/GRI_icKvRwc.png"
-        )
-        Framework.objects.create(
-            name="GRI: In accordance with", Image="images/framework/GRI_Vv3OtMS.png"
-        )
-        Framework.objects.create(name="CDP", Image="images/framework/CDP_6pVXNcF.png")
-        Framework.objects.create(name="BRSR", Image="images/framework/SEBI_ijaHMOS.png")
-        Framework.objects.create(name="SASB", Image="images/framework/SASB_doO0qoz.png")
-        Framework.objects.create(name="TCFD", Image="images/framework/TCFD_igK4bk3.png")
-        Framework.objects.create(
-            name="UNPRI", Image="images/framework/UNPRI_gLQQO0p.png"
-        )
-
-        Rating.objects.create(name="DJSI", Image="images/rating/DJSI_yZCyi9R.png")
-        Rating.objects.create(name="MSCI", Image="images/rating/MSCI_xXhLV4b.png")
-        Rating.objects.create(
-            name="SUSTAINALYTICS", Image="images/rating/SUSTAINALYTICS.png"
-        )
-
-        Regulation.objects.create(
-            name="SB-253", Image="images/regulation/SB-253_OjG0XhP.png"
-        )
-        Regulation.objects.create(
-            name="SB-261", Image="images/regulation/SB-261_1dJG1q5.png"
-        )
-        Regulation.objects.create(
-            name="BILL S-211", Image="images/regulation/Bill_S-211.png"
-        )
-        Regulation.objects.create(name="GHGRP", Image="images/regulation/GHGRP.png")
-
-        Sdg.objects.create(
-            name="No Poverty",
-            Image="images/sdg/E-WEB-Goal-01_w3iLQFp.png",
-            Target_no=7,
-            goal_no=1,
-        )
-        Sdg.objects.create(
-            name="Zero Hunger",
-            Image="images/sdg/E-WEB-Goal-02_PEdTrKf.png",
-            Target_no=7,
-            goal_no=2,
-        )
-        Sdg.objects.create(
-            name="Good Health & Well-Being",
-            Image="images/sdg/E-WEB-Goal-03_PcoVWT9.png",
-            Target_no=7,
-            goal_no=3,
-        )
-        Sdg.objects.create(
-            name="Quality Education",
-            Image="images/sdg/E-WEB-Goal-04_9ZpqV7e.png",
-            Target_no=7,
-            goal_no=4,
-        )
-        Sdg.objects.create(
-            name="Gender Equality",
-            Image="images/sdg/E-WEB-Goal-05_SM84Xa1.png",
-            Target_no=7,
-            goal_no=5,
-        )
-        Sdg.objects.create(
-            name="Clean Water & Sanitation",
-            Image="images/sdg/E-WEB-Goal-06_m3XADiN.png",
-            Target_no=7,
-            goal_no=6,
-        )
-        Sdg.objects.create(
-            name="Affordable & Clean Energy",
-            Image="images/sdg/E-WEB-Goal-07_eBXhTtg.png",
-            Target_no=7,
-            goal_no=7,
-        )
-        Sdg.objects.create(
-            name="Decent Work & Economic Growth",
-            Image="images/sdg/E-WEB-Goal-08_qsI9ovI.png",
-            Target_no=7,
-            goal_no=8,
-        )
-        Sdg.objects.create(
-            name="Industry, Innovation & Infrastructure",
-            Image="images/sdg/E-WEB-Goal-09_zv9AggO.png",
-            Target_no=7,
-            goal_no=9,
-        )
-        Sdg.objects.create(
-            name="Reduced Inequalities",
-            Image="images/sdg/E-WEB-Goal-10_VVSDI68.png",
-            Target_no=7,
-            goal_no=10,
-        )
-        Sdg.objects.create(
-            name="Sustainable Cities And Communities",
-            Image="images/sdg/E-WEB-Goal-11_8AuMlQR.png",
-            Target_no=7,
-            goal_no=11,
-        )
-        Sdg.objects.create(
-            name="Responsible Consumption An Production",
-            Image="images/sdg/E-WEB-Goal-12_3PW0ZHq.png",
-            Target_no=7,
-            goal_no=12,
-        )
-        Sdg.objects.create(
-            name="Climate Action",
-            Image="images/sdg/E-WEB-Goal-13_T2YCVhC.png",
-            Target_no=7,
-            goal_no=13,
-        )
-        Sdg.objects.create(
-            name="Life Below Water",
-            Image="images/sdg/E-WEB-Goal-14_BL41i3x.png",
-            Target_no=7,
-            goal_no=14,
-        )
-        Sdg.objects.create(
-            name="Life On Land",
-            Image="images/sdg/E-WEB-Goal-15_SSicEOp.png",
-            Target_no=7,
-            goal_no=15,
-        )
-        Sdg.objects.create(
-            name="Peace, Justice And Strong Institutions",
-            Image="images/sdg/E-WEB-Goal-16_E1KAPZA.png",
-            Target_no=7,
-            goal_no=16,
-        )
-        Sdg.objects.create(
-            name="Partnership For The Goals",
-            Image="images/sdg/E-WEB-Goal-17_5zRe7fI.png",
-            Target_no=7,
-            goal_no=17,
-        )
-
-        Target.objects.create(
-            name="SBTi", Image="images/Target/E-WEB-Target-01_w3iLQFp.png"
-        )
-        Target.objects.create(
-            name="SBTi Net Zero", Image="images/Target/SBTi_lnygnBi.png"
-        )
-
-        self.stdout.write(self.style.SUCCESS("Data loaded successfully"))
+ 
+    def update_or_create_sdgs(self):
+        sdgs = [
+            {
+                "id": 1,
+                "name": "No Poverty",
+                "Image": "images/sdg/E-WEB-Goal-01.png",
+                "Target_no": 7,
+                "goal_no": 1,
+            },
+            {
+                "id": 2,
+                "name": "Zero Hunger",
+                "Image": "images/sdg/E-WEB-Goal-02.png",
+                "Target_no": 7,
+                "goal_no": 2,
+            },
+            {
+                "id": 3,
+                "name": "Good Health & Well-Being",
+                "Image": "images/sdg/E-WEB-Goal-03.png",
+                "Target_no": 7,
+                "goal_no": 3,
+            },
+            {
+                "id": 4,
+                "name": "Quality Education",
+                "Image": "images/sdg/E-WEB-Goal-04.png",
+                "Target_no": 7,
+                "goal_no": 4,
+            },
+            {
+                "id": 5,
+                "name": "Gender Equality",
+                "Image": "images/sdg/E-WEB-Goal-05.png",
+                "Target_no": 7,
+                "goal_no": 5,
+            },
+            {
+                "id": 6,
+                "name": "Clean Water & Sanitation",
+                "Image": "images/sdg/E-WEB-Goal-06.png",
+                "Target_no": 7,
+                "goal_no": 6,
+            },
+            {
+                "id": 7,
+                "name": "Affordable & Clean Energy",
+                "Image": "images/sdg/E-WEB-Goal-07.png",
+                "Target_no": 7,
+                "goal_no": 7,
+            },
+            {
+                "id": 8,
+                "name": "Decent Work & Economic Growth",
+                "Image": "images/sdg/E-WEB-Goal-08.png",
+                "Target_no": 7,
+                "goal_no": 8,
+            },
+            {
+                "id": 9,
+                "name": "Industry, Innovation & Infrastructure",
+                "Image": "images/sdg/E-WEB-Goal-09.png",
+                "Target_no": 7,
+                "goal_no": 9,
+            },
+            {
+                "id": 10,
+                "name": "Reduced Inequalities",
+                "Image": "images/sdg/E-WEB-Goal-10.png",
+                "Target_no": 7,
+                "goal_no": 10,
+            },
+            {
+                "id": 11,
+                "name": "Sustainable Cities And Communities",
+                "Image": "images/sdg/E-WEB-Goal-11.png",
+                "Target_no": 7,
+                "goal_no": 11,
+            },
+            {
+                "id": 12,
+                "name": "Responsible Consumption An Production",
+                "Image": "images/sdg/E-WEB-Goal-12.png",
+                "Target_no": 7,
+                "goal_no": 12,
+            },
+            {
+                "id": 13,
+                "name": "Climate Action",
+                "Image": "images/sdg/E-WEB-Goal-13.png",
+                "Target_no": 7,
+                "goal_no": 13,
+            },
+            {
+                "id": 14,
+                "name": "Life Below Water",
+                "Image": "images/sdg/E-WEB-Goal-14.png",
+                "Target_no": 7,
+                "goal_no": 14,
+            },
+            {
+                "id": 15,
+                "name": "Life On Land",
+                "Image": "images/sdg/E-WEB-Goal-15.png",
+                "Target_no": 7,
+                "goal_no": 15,
+            },
+            {
+                "id": 16,
+                "name": "Peace, Justice And Strong Institutions",
+                "Image": "images/sdg/E-WEB-Goal-16.png",
+                "Target_no": 7,
+                "goal_no": 16,
+            },
+            {
+                "id": 17,
+                "name": "Partnership For The Goals",
+                "Image": "images/sdg/E-WEB-Goal-17.png",
+                "Target_no": 7,
+                "goal_no": 17,
+            },
+        ]
+        for sdg in sdgs:
+            Sdg.objects.update_or_create(id=sdg["id"], defaults=sdg)
+ 
+    def update_or_create_targets(self):
+        targets = [
+            {"id": 1, "name": "SBTi", "Image": "images/Target/SBTi.png"},
+            {"id": 2, "name": "SBTi Net Zero", "Image": "images/Target/SBTi.png"},
+        ]
+        for target in targets:
+            Target.objects.update_or_create(id=target["id"], defaults=target)
