67aaa20cea31da9284a8dad134a45f87218ee205
Utsav <utsav.pipersaniya@sustainext.ai>
2024-06-10 10:52:02 +0530

    added default authentication for the API

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -167,6 +167,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "EXCEPTION_HANDLER": "sustainapp.utils.custom_exception_handler",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 # JWT TOKEN LIFETIME
