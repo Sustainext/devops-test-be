545374d7e7dbd5d318c90cb4d2078c2725cf9268
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-06-19 17:06:58 +0530

    Added port in settings.
    Formatted the urls in datametric

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -110,6 +110,7 @@ DATABASES = {
         "HOST": os.environ.get("DBHOST"),
         "USER": os.environ.get("DBUSER"),
         "PASSWORD": os.environ.get("DBPASS"),
+        "PORT": int(os.environ.get("DBPORT")),
     }
 }
 
