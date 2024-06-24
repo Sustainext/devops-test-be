cf32ead699f3b89133bccba177a64dcd0a9151f1
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-06-24 15:57:24 +0530

    Created a mixin to export data as csv.
    Adding Export to CSV functionality in Path and RawResponse models.

--- datametric/admin.py
+++ datametric/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import MyModel, DataPoint, Path, FieldGroup, RawResponse, DataMetric
+from common.Mixins.ExportCsvMixin import ExportCsvMixin
 
 
 class MyModelAdmin(admin.ModelAdmin):
@@ -10,32 +11,34 @@ admin.site.register(MyModel, MyModelAdmin)
 
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("id","name", "created_at")
+    list_display = ("id", "name", "created_at")
 
 
 class DataMetricAdmin(admin.ModelAdmin):
-    list_display = ("id","name","path", "label", "description", "response_type")
+    list_display = ("id", "name", "path", "label", "description", "response_type")
 
 
 admin.site.register(DataMetric, DataMetricAdmin)
 
 
-class PathAdmin(admin.ModelAdmin):
-    list_display = ("id","name", "slug")
+class PathAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ("id", "name", "slug")
+    actions = ["export_as_csv"]
 
 
 admin.site.register(Path, PathAdmin)
 
 
 class FieldGroupAdmin(admin.ModelAdmin):
-    list_display = ("id","name", "path", "meta_data", "ui_schema", "schema")
+    list_display = ("id", "name", "path", "meta_data", "ui_schema", "schema")
 
 
 admin.site.register(FieldGroup, FieldGroupAdmin)
 
 
-class RawResponseAdmin(admin.ModelAdmin):
-    list_display = ("id","path", "created_at", "user", "client")
+class RawResponseAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ("id", "path", "created_at", "user", "client")
+    actions = ["export_as_csv"]
 
 
 admin.site.register(RawResponse, RawResponseAdmin)
