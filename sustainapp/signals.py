aefedd52064fa8b300961a9a437703479509fef9
Utsav <utsav.pipersaniya@sustainext.ai>
2024-07-10 13:30:06 +0530

    Fixes

--- sustainapp/signals.py
+++ sustainapp/signals.py
@@ -19,9 +19,8 @@ from django.contrib.auth.signals import user_logged_in
 from allauth.account.signals import user_signed_up
 from django.contrib.auth.models import update_last_login
 from sustainapp.models import LoginCounter
-from authentication.models import UserProfile
+from authentication.models import UserProfile, CustomUser
 import logging
-from authentication.models import CustomUser
 
 user_log = logging.getLogger("user_logger")
 
@@ -103,15 +102,3 @@ def check_password_change(sender, instance, created, **kwargs):
                 login_counter = instance.first_login
                 login_counter.needs_password_change = False
                 login_counter.save()
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def update_client_id(sender, instance, **kwargs):
-    if (
-        hasattr(instance, "client")
-        and instance.client is not None
-        and instance.client.id is not None
-    ):
-        UserProfile.objects.update_or_create(
-            user=instance, defaults={"client_id": instance.client.id}
-        )
