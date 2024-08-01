cb5900f6ae700e8c208333c496fdd1e98e530689
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-08-01 11:25:36 +0530

    Fixed response for Community Development

--- sustainapp/Views/Analyse/Social/CommunityDevelopmentAnalyse.py
+++ sustainapp/Views/Analyse/Social/CommunityDevelopmentAnalyse.py
@@ -41,33 +41,7 @@ class SocialCommunityDevelopmentAnalysis(APIView):
         )
 
     def get_community_engagement(self):
-        #* Making Response like this
-        [
-            {
-                "": "Percentage of Security Personnel (in organisation)",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-            {
-                "": "Environmental impact assessments",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-            {
-                "": "Public disclosure",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-            {
-                "": "Community development programs",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-            {
-                "": "Stakeholder engagement plans",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-            {
-                "": "Local community consultation committes",
-                "Percentage of operations implemented by engaging local communities": "",
-            },
-        ]
+        # * Making Response like this
         """
         This function is used to get the community engagement.
         """
@@ -76,14 +50,14 @@ class SocialCommunityDevelopmentAnalysis(APIView):
             for raw_response in self.raw_responses.filter(path__slug=self.slugs[0])
         ]
         response_data = {
-            "social_impact_assessments": 0,
-            "environmental_impact_assessments": 0,
-            "public_disclosure": 0,
-            "community_development_programs": 0,
-            "stakeholder_engagement_plans": 0,
-            "local_community_consultation_committes": 0,
-            "works_councils_occupational_health_and_safety_committees": 0,
-            "community_grievance_processes": 0,
+            "Social impact assessments": 0,
+            "Environmental impact assessments": 0,
+            "Public disclosure": 0,
+            "Community development programs": 0,
+            "Stakeholder engagement plans": 0,
+            "Local community consultation committes": 0,
+            "works councils, occupational health and safety committees": 0,
+            "Community grievance processes": 0,
         }
         sum_of_total_operation = sum(
             [
@@ -93,31 +67,39 @@ class SocialCommunityDevelopmentAnalysis(APIView):
         )
 
         for each_month_data in local_data:
-            response_data["social_impact_assessments"] += int(
+            response_data["Social impact assessments"] += int(
                 each_month_data[0]["operations"]
             )
-            response_data["environmental_impact_assessments"] += int(
+            response_data["Environmental impact assessments"] += int(
                 each_month_data[1]["operations"]
             )
-            response_data["public_disclosure"] += int(each_month_data[2]["operations"])
-            response_data["community_development_programs"] += int(
+            response_data["Public disclosure"] += int(each_month_data[2]["operations"])
+            response_data["Community development programs"] += int(
                 each_month_data[3]["operations"]
             )
-            response_data["stakeholder_engagement_plans"] += int(
+            response_data["Stakeholder engagement plans"] += int(
                 each_month_data[4]["operations"]
             )
-            response_data["local_community_consultation_committes"] += int(
+            response_data["Local community consultation committes"] += int(
                 each_month_data[5]["operations"]
             )
             response_data[
-                "works_councils_occupational_health_and_safety_committees"
+                "works councils, occupational health and safety committees"
             ] += int(each_month_data[6]["operations"])
-            response_data["community_grievance_processes"] += int(
+            response_data["Community grievance processes"] += int(
                 each_month_data[7]["operations"]
             )
+        response_data_list = []
         for key, value in response_data.items():
             response_data[key] = safe_integer_divide(value, sum_of_total_operation)
-        return response_data
+        for key, value in response_data.items():
+            response_data_list.append(
+                {
+                    "": key,
+                    "Percentage of operations implemented by engaging local communities": value,
+                }
+            )
+        return response_data_list
 
     def get(self, request, format=None):
         """
