69f1042ad08ae73d7d3736f0ee482ea11476a728
Utsavsustainx <utsav.pipersaniya@sustainext.ai>
2024-08-28 15:08:01 +0530

    Business details API

--- datametric/middleware/GeneralBusinessDetails.py
+++ datametric/middleware/GeneralBusinessDetails.py
@@ -1,9 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
 from datametric.models import RawResponse,Path
 from urllib.parse import urlparse, parse_qs
-from sustainapp.models import Organization, Corporateentity
+from sustainapp.models import Organization, Corporateentity,Client
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.exceptions import PermissionDenied
+from django.conf import settings
+import jwt
 
 class PathSlugMiddleware(MiddlewareMixin):
     def get_sector_data(self, request, organization_id,corporate_id):
@@ -14,7 +17,6 @@ class PathSlugMiddleware(MiddlewareMixin):
             sector_subindustry_data = []
             for corporate_entity in corporate_entity:
                 sector_subindustry_data.append({
-                    'corporate_name': corporate_entity.name,
                     'Sector': corporate_entity.sector,
                     'Sub_industry': corporate_entity.subindustry
                 })
@@ -25,22 +27,29 @@ class PathSlugMiddleware(MiddlewareMixin):
             sector_subindustry_data = []
             for org in organization:
                 sector_subindustry_data.append({
-                    'organization_name':org.name,
                     'Sector': org.sector,
                     'Sub_industry': org.subindustry
                 })
             for corporate_entity in corporate_entities:
                 sector_subindustry_data.append({
-                    'corporate_name': corporate_entity.name,
                     'Sector': corporate_entity.sector,
                     'Sub_industry': corporate_entity.subindustry
                 })
             return sector_subindustry_data
         
     def process_view(self, request, view_func, view_args, view_kwargs):
-        print(f"User: {request.user}, Authenticated: {request.user.is_authenticated}")
-        
         if request.method == 'GET' and request.path == '/datametric/get-fieldgroups':
+            auth_header = request.headers.get("Authorization")
+            if auth_header:
+                    token = auth_header.split(" ")[1]
+                    payload = jwt.decode(
+                        token,
+                        settings.SECRET_KEY,
+                        algorithms=["HS256"],
+                    )
+                    client_head = payload.get("client_id")
+                    user_head = payload.get("user_id")
+                    
             query_params = parse_qs(urlparse(request.get_full_path()).query)
             path_name = query_params.get('path_slug', [None])[0]
 
@@ -50,12 +59,14 @@ class PathSlugMiddleware(MiddlewareMixin):
                 organization = request.GET.get('organisation')
                 corporate = request.GET.get('corporate')
                 year = request.GET.get('year')
-                user = request.user
-                client = user.client_id
+                user = user_head
+                client = client_head
                 data = self.get_sector_data(request, organization, corporate)
                 
                 # Fetch the Path instance
                 path_instance = Path.objects.get(slug=path_name)
+                if corporate =='':
+                    corporate = None
 
                 # Check if a RawResponse with the same filter criteria already exists
                 if not RawResponse.objects.filter(
@@ -63,7 +74,7 @@ class PathSlugMiddleware(MiddlewareMixin):
                     corporate_id=corporate,
                     year=year,
                     path=path_instance,
-                    user=user,
+                    user_id=user,
                     client_id=client
                 ).exists():
                     # If it doesn't exist, create a new RawResponse entry
@@ -72,12 +83,13 @@ class PathSlugMiddleware(MiddlewareMixin):
                         corporate_id=corporate,
                         year=year,
                         path=path_instance,
-                        user=user,
+                        user_id=user,
                         client_id=client,
                         data=data
                     )
-                    print("PathSlugMiddleware: RawResponse created")
+                    # print("PathSlugMiddleware: RawResponse created")
                 else:
-                    print("PathSlugMiddleware: RawResponse already exists")
+                    None
+                    # print("PathSlugMiddleware: RawResponse already exists")
 
         return None
