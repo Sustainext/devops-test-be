fbf81a30b769013f05af2eeb44ac592c0dff3926
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-09-05 09:39:36 +0530

    Fixed Materiality modelviewset

--- materiality_dashboard/Views/MaterialityAssesmentModelViewset.py
+++ materiality_dashboard/Views/MaterialityAssesmentModelViewset.py
@@ -13,10 +13,4 @@ class MaterialityAssessmentViewSet(viewsets.ModelViewSet):
         return MaterialityAssessment.objects.filter(client=self.request.user.client)
 
     def perform_create(self, serializer):
-        #* Add a validation that checks the time period of the assessment with the framework is not overlapping with any in_progress assessment
-        start_date = serializer.validated_data['start_date']
-        end_date = serializer.validated_data['end_date']
-        if MaterialityAssessment.objects.filter(client=self.request.user.client, status="in_progress",start_date__gte=start_date,end_date__lte=end_date,framework=serializer.validated_data["framework"]).exists():
-            raise serializers.ValidationError({"error": "Client already has an in progress assessment."})
-
         serializer.save(client=self.request.user.client)
