e66f048288177923c23c216e587921540ea17f1a
Yashwanth39 <yashwanth.vanimina@sustainext.ai>
2024-09-16 11:46:27 +0530

    Added comments for more redability

--- sustainapp/Views/Analyse/SupplierEnvironment/SupplierEnvironment.py
+++ sustainapp/Views/Analyse/SupplierEnvironment/SupplierEnvironment.py
@@ -9,27 +9,28 @@ from logging import getLogger
 
 logger = getLogger("error.log")
 
-class SupplierEnvAnlayzeView(APIView) :
+
+class SupplierEnvAnlayzeView(APIView):
 
     permission_classes = [IsAuthenticated]
 
     def calculate_percentage(self, q1, q2=None, filter_for_corp=False):
-
+        """Calculate the percentage of the given values. filter_for_corp when true indicates that the user has selected to view by Organization, But the organization has no data, So we are checking for it's corporates"""
         if not filter_for_corp:
 
             return {
-                "org_or_corp" : self.org.name if not self.corp else self.corp.name,
-                "percentage" : (q1/q2)*100 if q2 !=0 else 0
+                "org_or_corp": self.org.name if not self.corp else self.corp.name,
+                "percentage": (q1 / q2) * 100 if q2 != 0 else 0,
             }
-        
-        else :
+
+        else:
 
             return {
-                "org_or_corp" : self.corprorate_of_org.name,
-                "percentage" : (q1/q2)*100 if q2 !=0 else 0
+                "org_or_corp": self.corprorate_of_org.name,
+                "percentage": (q1 / q2) * 100 if q2 != 0 else 0,
             }
 
-    def new_suppliers(self, path, filter_by, filter_for_corp=False) :
+    def new_suppliers(self, path, filter_by, filter_for_corp=False):
 
         lst = []
 
@@ -40,62 +41,96 @@ class SupplierEnvAnlayzeView(APIView) :
             year=self.year,
         ).first()
 
-        if sup_env_data and path == "gri-supplier_environmental_assessment-new_suppliers-308-1a" :
-
-            try :
+        if (
+            sup_env_data
+            and path == "gri-supplier_environmental_assessment-new_suppliers-308-1a"
+        ):
+            """When the if condition is true, it means that path should have 2 fields named Q1 and Q2. 
+            We calculate the percentage using them, it any one field is not added we return empty list"""
+            try:
                 q1 = sup_env_data.data[0]["Q1"]
                 q2 = sup_env_data.data[0]["Q2"]
-                lst.append(self.calculate_percentage(float(q1), float(q2),filter_for_corp=filter_for_corp))
-            except Exception as e :
-                logger.error(f"SupplierEnvironment.py > the exception {e} for the path {path}")
+                lst.append(
+                    self.calculate_percentage(
+                        float(q1), float(q2), filter_for_corp=filter_for_corp
+                    )
+                )
+            except Exception as e:
+                logger.error(
+                    f"SupplierEnvironment.py > the exception {e} for the path {path}"
+                )
                 return []
             return lst
 
-        elif sup_env_data and path in ["gri-supplier_environmental_assessment-negative_environmental-308-2d","gri-supplier_environmental_assessment-negative_environmental-308-2e"]:
-
-            try :
-                self.suppliers_assesed = RawResponse.objects.filter(
-                    **filter_by,
-                    path__slug="gri-supplier_environmental_assessment-negative_environmental-308-2a",
-                    client_id=self.client_id,
-                    year=self.year,
-                ).first().data[0]["Q1"]
+        elif sup_env_data and path in [
+            "gri-supplier_environmental_assessment-negative_environmental-308-2d",
+            "gri-supplier_environmental_assessment-negative_environmental-308-2e",
+        ]:
+            """When this elif expression is true, we only have one field (Q1) in the data, so the other field which is required to calculate the percentage is the self.suppliers_assesed."""
+            try:
+                self.suppliers_assesed = (
+                    RawResponse.objects.filter(
+                        **filter_by,
+                        path__slug="gri-supplier_environmental_assessment-negative_environmental-308-2a",
+                        client_id=self.client_id,
+                        year=self.year,
+                    )
+                    .first()
+                    .data[0]["Q1"]
+                )
 
                 q1 = sup_env_data.data[0]["Q1"]
-                lst.append(self.calculate_percentage(float(q1), float(self.suppliers_assesed),filter_for_corp=filter_for_corp))
-            except Exception as e :
-                logger.error(f"SupplierEnvironment.py > the exception {e} for the path {path}")
+                lst.append(
+                    self.calculate_percentage(
+                        float(q1),
+                        float(self.suppliers_assesed),
+                        filter_for_corp=filter_for_corp,
+                    )
+                )
+            except Exception as e:
+                logger.error(
+                    f"SupplierEnvironment.py > the exception {e} for the path {path}"
+                )
                 return []
             return lst
-        
-        elif not sup_env_data and self.corp is None :
 
+        elif not sup_env_data and self.corp is None:
+            """If there is no data when the Organization alone is selected, then we enter this case.
+            Here we loop through all it's Corporates to get the data"""
+
+            """Here we make self.corp to True as a break condition for the recurssion,
+            before we get out of this looping through all the corporates we will make self.corp to None
+            which would be it's initial state. By this the next path can check if it's Organization has
+            no data, So that it should check for it's corporates."""
             self.corp = True
+
             corps_of_org = Corporateentity.objects.filter(organization=self.org)
             result = []
             for corporate in corps_of_org:
-                logger.error(f"SupplierEnvironment.py > checking for the corporate {corporate.name} with the path {path}")
+                logger.error(
+                    f"SupplierEnvironment.py > checking for the corporate {corporate.name} with the path {path}"
+                )
                 self.corprorate_of_org = corporate
                 filtering = {
-                    "organization__id" : self.org.id,
-                    "corporate__id" : corporate.id
+                    "organization__id": self.org.id,
+                    "corporate__id": corporate.id,
                 }
-
+                """Pass an additional parameter to tell the calculate_percentage function that  we are getting the corporates of the Organization since no data was found in the organization""" 
                 k = self.new_suppliers(path, filtering, filter_for_corp=True)
                 if k != []:
                     result.extend(k)
             self.corp = None
             return result
-        
-        else :
+
+        else:
             # No data exits
             return []
 
-    def get (self, request ) :
+    def get(self, request):
 
         serialized_data = CheckOrgCoprDateSerializer(data=request.query_params)
         serialized_data.is_valid(raise_exception=True)
-        try :
+        try:
             self.org = serialized_data.validated_data["organisation"]
             self.corp = serialized_data.validated_data.get("corporate", None)
             self.from_date = serialized_data.validated_data["start"]
@@ -132,16 +167,19 @@ class SupplierEnvAnlayzeView(APIView) :
 
             supplier_env_data = {}
 
-            supplier_env_data['gri_308_1a'] = self.new_suppliers(
+            supplier_env_data["gri_308_1a"] = self.new_suppliers(
                 "gri-supplier_environmental_assessment-new_suppliers-308-1a", filter_by
             )
 
             supplier_env_data["gri_308_2d"] = self.new_suppliers(
-                    "gri-supplier_environmental_assessment-negative_environmental-308-2d",
-                    filter_by
-                )
+                "gri-supplier_environmental_assessment-negative_environmental-308-2d",
+                filter_by,
+            )
 
-            supplier_env_data['gri_308_2e'] = self.new_suppliers("gri-supplier_environmental_assessment-negative_environmental-308-2e", filter_by) 
+            supplier_env_data["gri_308_2e"] = self.new_suppliers(
+                "gri-supplier_environmental_assessment-negative_environmental-308-2e",
+                filter_by,
+            )
 
             return Response(
                 supplier_env_data,
