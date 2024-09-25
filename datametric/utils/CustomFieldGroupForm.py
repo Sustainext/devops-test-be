23d787f26ddb4ac615860309f0893b52526c5002
Utsavsustainx <utsav.pipersaniya@sustainext.ai>
2024-09-25 10:06:48 +0530

    Fixing fieldgroup custom form

--- datametric/utils/CustomFieldGroupForm.py
+++ datametric/utils/CustomFieldGroupForm.py
@@ -8,5 +8,15 @@ class FieldGroupAdminForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(FieldGroupAdminForm, self).__init__(*args, **kwargs)
-        # Exclude Path instances that are already linked to a FieldGroup
-        self.fields['path'].queryset = Path.objects.exclude(id__in=FieldGroup.objects.values('path_id'))
+        
+        # Check if we're editing an existing FieldGroup instance
+        if self.instance and self.instance.pk:
+            # Exclude paths linked to other FieldGroups, but include the one linked to the current instance
+            self.fields['path'].queryset = Path.objects.exclude(
+                id__in=FieldGroup.objects.exclude(pk=self.instance.pk).values('path_id')
+            )
+        else:
+            # If it's a new instance, exclude all paths already linked to a FieldGroup
+            self.fields['path'].queryset = Path.objects.exclude(
+                id__in=FieldGroup.objects.values('path_id')
+            )
