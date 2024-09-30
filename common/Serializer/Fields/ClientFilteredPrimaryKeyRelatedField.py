a8f29e4e85eb7b40d5221c4c36afa6971ec99eaf
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-09-30 15:31:01 +0530

    Added ScreenTwo API Views, Serializers and Models

---
+++ common/Serializer/Fields/ClientFilteredPrimaryKeyRelatedField.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+
+class ClientFilteredPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    """
+    Custom PrimaryKeyRelatedField that filters the queryset based on the user's client.
+    """
+
+    def get_queryset(self):
+        request = self.context.get("request", None)
+        queryset = super().get_queryset()
+
+        # Ensure the request and user are available in the context
+        if request and hasattr(request.user, "client"):
+            # Filter the queryset based on the user's client
+            return queryset.filter(client=request.user.client)
+
+        return queryset.none()  # Return an empty queryset if no request or client
--- esg_report/urls.py
+++ esg_report/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from esg_report.Views.ScreenOne import ScreenOneView
-
+from esg_report.Views.ScreenTwo import ScreenTwo
 
 router = DefaultRouter()
 
 urlpatterns = [
     path(
-        "esg_report/<int:esg_report_id>/screen_one/",
+        "screen_one/<int:esg_report_id>/",
         ScreenOneView.as_view(),
         name="screen_one",
+    ),
+    path(
+        "screen_two/<int:report_id>/",
+        ScreenTwo.as_view(),
+        name="screen_two",
     ),
 ]
 urlpatterns += router.urls
