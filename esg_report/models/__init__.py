8f1d0f0d82d7ed2858fff7534439ed2f17132753
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-09-30 18:06:51 +0530

    Added Screen Four Model

--- esg_report/models/__init__.py
+++ esg_report/models/__init__.py
@@ -2,3 +2,4 @@ from .ESGReport import ESGReport
 from .ScreenOne import CeoMessage
 from .ScreenTwo import AboutTheCompanyAndOperations
 from .ScreenThree import MissionVisionValues
+from .ScreenFour import SustainabilityRoadmap
