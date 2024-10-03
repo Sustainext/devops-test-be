d2a7fcf3c9371b82ac78a3ad4a1aeb5befa7f44a
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-03 09:51:41 +0530

    Added Changes to Screen One

--- esg_report/Serializer/CeoMessageSerializer.py
+++ esg_report/Serializer/CeoMessageSerializer.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from esg_report.models.ScreenOne import CeoMessage
+from common.Serializer.Fields.ClientFilteredPrimaryKeyRelatedField import (
+    ClientFilteredPrimaryKeyRelatedField,
+)
+from sustainapp.models import Report
 
 
 class CeoMessageSerializer(serializers.ModelSerializer):
     """
     Serializer for the CEO Message model
     """
+
+    report = ClientFilteredPrimaryKeyRelatedField(
+        queryset=Report.objects.all(), required=False
+    )
 
     class Meta:
         model = CeoMessage
