2c94caaee48b3b785ae8baf518563202011c7a66
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-03 17:04:34 +0530

    Added TextField instead of ImageField

--- esg_report/utils.py
+++ esg_report/utils.py
@@ -8,7 +8,17 @@ def get_latest_raw_response(raw_responses, slug):
 
 
 def get_materiality_dashboard(report: Report):
-    try:
-        materiality_assessment = MaterialityAssessment.objects.get(client=report.client)
-    except MaterialityAssessment.DoesNotExist:
-        raise ValidationError("Materiality assessment not found for the client.")
+    """
+    x < a,  y < b :
+    Case 1 - [Previous Materiality Exists] Use the material topics from m₁ OR the previous materiality assessment done within the dates where 'x' and 'y' fall.
+    Case 2 - [Previous Materiality does not exist] Validation needs to be shown that no material topics have been selected for the chosen dates. User cannot proceed without selecting material topics.
+
+    x < a,  y > b : Use the material topics from m₂.
+
+    x > a,  y < b : Use the material topics from m₂.
+
+    x > a,  y > b :
+    Case 1 (if x<b) -  Use the material topics from m₂, show message that "Start and end date chosen for the report fall outside the dates applicable to the current Materiality Assessment. Proceed with the most recent material topics?"
+
+    Case 2 (if x>b) - Validation needs to be shown that no material topics have been selected for the chosen dates. User cannot proceed without selecting material topics.
+    """
