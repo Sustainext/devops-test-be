894f57af891f468620ad05a6bf0bf85a214d2d0e
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-04 10:57:05 +0530

    Removed Validations from screen one and screen two

--- esg_report/Views/ScreenTwo.py
+++ esg_report/Views/ScreenTwo.py
@@ -18,8 +18,13 @@ class ScreenTwo(APIView):
         try:
             # Fetch report
             report = Report.objects.get(id=report_id)
+        except Report.DoesNotExist as e:
+            return Response(
+                {"error": "Report does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
 
-            # Fetch AboutTheCompanyAndOperations for the report
+        # Fetch AboutTheCompanyAndOperations for the report
+        try:
             about_the_company_and_operations = AboutTheCompanyAndOperations.objects.get(
                 report=report
             )
@@ -27,95 +32,93 @@ class ScreenTwo(APIView):
                 about_the_company_and_operations, context={"request": request}
             )
             response_data = serializer.data
-
-            # Define slugs for GRI questions
-            slugs = {
-                "org_details": "gri-general-org_details_2-1a-1b-1c-1d",
-                "entities": "gri-general-entities-list_of_entities-2-2-a",
-                "sectors": "gri-general-business_details-organisation-2-6a",
-                "value_chain": "gri-general-business_details-value-2-6b",
-                "relevant_business": "gri-general-business_details-other-2-6c",
-                "changes": "gri-general-business_details-changes-2-6d",
+        except AboutTheCompanyAndOperations.DoesNotExist as e:
+            response_data = {
+                "report": report.id,
+                "about_the_company":"",
+                "business_relations":"",
+                "entities_included":"",
+                "supply_chain_description":""
             }
 
-            # Fetch all raw responses once, filter by the year range and client
-            raw_responses = RawResponse.objects.filter(
-                client=request.user.client,
-                year__range=(report.start_date.year, report.end_date.year),
-            )
+        # Define slugs for GRI questions
+        slugs = {
+            "org_details": "gri-general-org_details_2-1a-1b-1c-1d",
+            "entities": "gri-general-entities-list_of_entities-2-2-a",
+            "sectors": "gri-general-business_details-organisation-2-6a",
+            "value_chain": "gri-general-business_details-value-2-6b",
+            "relevant_business": "gri-general-business_details-other-2-6c",
+            "changes": "gri-general-business_details-changes-2-6d",
+        }
 
-            # Fetch raw responses and update the response data
-            raw_response_org_details = get_latest_raw_response(
-                raw_responses=raw_responses, slug=slugs["org_details"]
-            )
-            if raw_response_org_details:
-                response_data["2-1"] = {
-                    "legal_name": raw_response_org_details.data[0]["Q1"]["text"],
-                    "nature_of_ownership_and_legal_form": raw_response_org_details.data[
-                        0
-                    ]["Q2"]["text"],
-                    "location_of_headquarters": raw_response_org_details.data[0]["Q3"][
-                        "text"
-                    ],
-                    "countries_of_operation": [
-                        i["text"]
-                        for i in raw_response_org_details.data[0]["Q4"]["rows"]
-                    ],
-                }
-            else:
-                response_data["2-1"] = None
+        # Fetch all raw responses once, filter by the year range and client
+        raw_responses = RawResponse.objects.filter(
+            client=request.user.client,
+            year__range=(report.start_date.year, report.end_date.year),
+        )
 
-            raw_response_entities = get_latest_raw_response(
-                raw_responses, slugs["entities"]
-            )
-            if raw_response_entities:
-                response_data["2-2-a"] = [
-                    entity["Q1"] for entity in raw_response_entities.data
-                ]
-            else:
-                response_data["2-2-a"] = None
+        # Fetch raw responses and update the response data
+        raw_response_org_details = get_latest_raw_response(
+            raw_responses=raw_responses, slug=slugs["org_details"]
+        )
+        if raw_response_org_details:
+            response_data["2-1"] = {
+                "legal_name": raw_response_org_details.data[0]["Q1"]["text"],
+                "nature_of_ownership_and_legal_form": raw_response_org_details.data[0][
+                    "Q2"
+                ]["text"],
+                "location_of_headquarters": raw_response_org_details.data[0]["Q3"][
+                    "text"
+                ],
+                "countries_of_operation": [
+                    i["text"] for i in raw_response_org_details.data[0]["Q4"]["rows"]
+                ],
+            }
+        else:
+            response_data["2-1"] = None
 
-            raw_response_sectors = get_latest_raw_response(
-                raw_responses=raw_responses, slug=slugs["sectors"]
-            )
-            if raw_response_sectors:
-                response_data["2-6-a"] = raw_response_sectors.data
-            else:
-                response_data["2-6-a"] = None
+        raw_response_entities = get_latest_raw_response(
+            raw_responses, slugs["entities"]
+        )
+        if raw_response_entities:
+            response_data["2-2-a"] = [
+                entity["Q1"] for entity in raw_response_entities.data
+            ]
+        else:
+            response_data["2-2-a"] = None
 
-            raw_response_value_chain = get_latest_raw_response(
-                raw_responses=raw_responses, slug=slugs["value_chain"]
-            )
-            if raw_response_value_chain:
-                response_data["2-6-b"] = raw_response_value_chain.data[0]
-            else:
-                response_data["2-6-b"] = None
+        raw_response_sectors = get_latest_raw_response(
+            raw_responses=raw_responses, slug=slugs["sectors"]
+        )
+        if raw_response_sectors:
+            response_data["2-6-a"] = raw_response_sectors.data
+        else:
+            response_data["2-6-a"] = None
 
-            raw_response_relevant_business = get_latest_raw_response(
-                raw_responses=raw_responses, slug=slugs["relevant_business"]
-            )
-            if raw_response_relevant_business:
-                response_data["2-6-c"] = raw_response_relevant_business.data[0]["Q1"]
-            else:
-                response_data["2-6-c"] = None
+        raw_response_value_chain = get_latest_raw_response(
+            raw_responses=raw_responses, slug=slugs["value_chain"]
+        )
+        if raw_response_value_chain:
+            response_data["2-6-b"] = raw_response_value_chain.data[0]
+        else:
+            response_data["2-6-b"] = None
 
-            raw_response_change_information = get_latest_raw_response(
-                raw_responses=raw_responses, slug=slugs["changes"]
-            )
-            if raw_response_change_information:
-                response_data["2-6-d"] = raw_response_change_information.data[0]["Q1"]
-            else:
-                response_data["2-6-d"] = None
-            return Response(response_data, status=status.HTTP_200_OK)
-        except Report.DoesNotExist as e:
-            return Response(
-                {"error": "Report does not exist"}, status=status.HTTP_404_NOT_FOUND
-            )
-        except AboutTheCompanyAndOperations.DoesNotExist as e:
-            return Response(
-                None,
-                status=status.HTTP_200_OK,
-            )
+        raw_response_relevant_business = get_latest_raw_response(
+            raw_responses=raw_responses, slug=slugs["relevant_business"]
+        )
+        if raw_response_relevant_business:
+            response_data["2-6-c"] = raw_response_relevant_business.data[0]["Q1"]
+        else:
+            response_data["2-6-c"] = None
+
+        raw_response_change_information = get_latest_raw_response(
+            raw_responses=raw_responses, slug=slugs["changes"]
+        )
+        if raw_response_change_information:
+            response_data["2-6-d"] = raw_response_change_information.data[0]["Q1"]
+        else:
+            response_data["2-6-d"] = None
+        return Response(response_data, status=status.HTTP_200_OK)
 
     def put(self, request, report_id, format=None):
         try:
