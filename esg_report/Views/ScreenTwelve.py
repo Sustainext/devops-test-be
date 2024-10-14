3a288d1001bdfc43e2bd4e62ae0d0622cd144d56
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-14 09:21:44 +0530

    Added Material methods in Screen Twelve

--- esg_report/Views/ScreenTwelve.py
+++ esg_report/Views/ScreenTwelve.py
@@ -6,11 +6,17 @@ from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
 from esg_report.Serializer.ScreenTwelveSerializer import ScreenTwelveSerializer
 from sustainapp.models import Report
+from collections import defaultdict
 from datametric.models import RawResponse
 from esg_report.utils import (
     get_materiality_assessment,
     get_raw_responses_as_per_report,
     get_data_points_as_per_report,
+)
+from datametric.utils.analyse import set_locations_data
+from sustainapp.Utilities.emission_analyse import (
+    get_top_emission_by_scope,
+    calculate_scope_contribution,
 )
 
 
@@ -44,13 +50,87 @@ class ScreenTwelveAPIView(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.slugs = {}
+        self.slugs = {
+            0: "gri-environment-emissions-301-a-scope-1",
+            1: "gri-environment-emissions-301-a-scope-2",
+            2: "gri-environment-emissions-301-a-scope-3",
+            3: "gri-environment-materials-301-1a-non_renewable_materials",
+        }
+
+    def get_301_1a_non_renewable_materials(self):
+        local_data_points = self.data_points.filter(path__slug=self.slugs[3]).order_by(
+            "index"
+        )
+        response_data = defaultdict(dict)
+        response_data = {
+            index: dict() for index in local_data_points.values_list("index", flat=True)
+        }
+        for data_point in local_data_points:
+            response_data[data_point.index][
+                data_point.data_metric.name
+            ] = data_point.value
+        return list(response_data.values())
+
+    def get_301_123_collect(self):
+        slugs = [self.slugs[0], self.slugs[1], self.slugs[2]]
+        data_points = self.data_points.filter(path__slug__in=slugs)
+        slug_data = defaultdict(list)
+        for slug in slugs:
+            response_data = defaultdict(list)
+            for data_point in data_points.filter(path__slug=slug):
+                response_data[data_point.data_metric.name] = data_point.value
+            slug_data[slug] = response_data
+        return slug_data
+
+    def get_301_123_analyse(self):
+        locations = set_locations_data(
+            organisation=self.report.organization,
+            corporate=self.report.corporate,
+        )
+        top_emission_by_scope, top_emission_by_source, top_emission_by_location = (
+            get_top_emission_by_scope(
+                locations=locations,
+                user=self.request.user,
+                start=self.report.start_date,
+                end=self.report.end_date,
+                path_slug={
+                    "gri-environment-emissions-301-a-scope-1": "Scope 1",
+                    "gri-environment-emissions-301-a-scope-2": "Scope 2",
+                    "gri-environment-emissions-301-a-scope-3": "Scope 3",
+                },
+            )
+        )
+
+        # * Prepare response data
+        response_data = dict()
+        response_data["all_emission_by_scope"] = calculate_scope_contribution(
+            key_name="scope", scope_total_values=top_emission_by_scope
+        )
+        response_data["all_emission_by_source"] = calculate_scope_contribution(
+            key_name="source", scope_total_values=top_emission_by_source
+        )
+        response_data["all_emission_by_location"] = calculate_scope_contribution(
+            key_name="location", scope_total_values=top_emission_by_location
+        )
+        response_data["top_5_emisson_by_source"] = response_data[
+            "all_emission_by_source"
+        ][0:5]
+        response_data["top_5_emisson_by_location"] = response_data[
+            "all_emission_by_location"
+        ][0:5]
+        return response_data
+
+    def get_305_4abc(self):
+        # TODO: Need more clarification from Sakthivel
+        return None
+
+    def get_305_5abc(self):
+        # TODO: Need more clarification from Sakthivel
+        return None
 
     def get_3_3cde(self):
-        #TODO: Materiality Assessment Screen is pending
+        # TODO: Materiality Assessment Screen is pending
         return None
-    
-    
 
     def get(self, request, report_id):
         try:
@@ -75,5 +155,14 @@ class ScreenTwelveAPIView(APIView):
 
         self.set_raw_responses()
         self.set_data_points()
+        response_data["3_3cde"] = self.get_3_3cde()
+        response_data["305_123_collect"] = self.get_301_123_collect()
+        response_data["305_123_analyse"] = self.get_301_123_analyse()
+        response_data["305_4abc"] = self.get_305_4abc()
+        response_data["305_5abc"] = self.get_305_5abc()
+        response_data["301_1a_non_renewable_materials"] = (
+            self.get_301_1a_non_renewable_materials()
+        )
+        
 
         return Response(response_data, status=status.HTTP_200_OK)
--- sustainapp/Views/EmissionAnalyse.py
+++ sustainapp/Views/EmissionAnalyse.py
@@ -1,5 +1,3 @@
-from datametric.models import DataPoint, Path, DataMetric, RawResponse
-from sustainapp.models import Organization, Corporateentity, Location
 from rest_framework.views import APIView
 from collections import defaultdict
 from rest_framework.response import Response
@@ -9,11 +7,10 @@ from sustainapp.Serializers.CheckAnalysisViewSerializer import (
     CheckAnalysisViewSerializer,
 )
 from datametric.utils.analyse import set_locations_data
-from operator import itemgetter
-from datametric.utils.analyse import filter_by_start_end_dates
-
-from django.db.models import Prefetch
-from rest_framework import serializers
+from sustainapp.Utilities.emission_analyse import (
+    calculate_scope_contribution,
+    get_top_emission_by_scope,
+)
 
 
 class GetEmissionAnalysis(APIView):
@@ -24,96 +21,63 @@ class GetEmissionAnalysis(APIView):
         "gri-environment-emissions-301-a-scope-3": "Scope 3",
     }
 
-    def calculate_scope_contribution(self, key_name, scope_total_values):
-        total_emissions = sum(scope_total_values.values())
-        scope_contributions = []
-        for scope_name, scope_value in scope_total_values.items():
-            try:
-                contribution = (scope_value / total_emissions) * 100
-            except ZeroDivisionError:
-                contribution = 0
-            scope_contributions.append(
-                {
-                    key_name: scope_name,
-                    "total": scope_value,
-                    "contribution": round(contribution, 3),
-                    "Units": "tC02e",
-                }
-            )
-        scope_contributions.sort(key=itemgetter("contribution"), reverse=True)
-        return scope_contributions
-
-    def get_top_emission_by_scope(self):
-        # * Get all Raw Respones based on location and year.
-        self.data_points = DataPoint.objects.filter(
-            json_holder__isnull=False,
-            is_calculated=True,
-            path__slug__icontains="gri-collect-emissions-scope-combined",
-            locale__in=self.locations,  # .values_list("name", flat=True),
-            client_id=self.request.user.client.id,
-        ).select_related("raw_response").filter(
-            filter_by_start_end_dates(start_date=self.start, end_date=self.end)
-        )
-        # * Get contribution of each path in raw_responses and sum the json_holder
-        top_emission_by_scope = defaultdict(lambda: 0)
-        self.top_emission_by_source = defaultdict(lambda: 0)
-        self.top_emission_by_location = defaultdict(lambda: 0)
-        for data_point in self.data_points:
-            top_emission_by_scope[
-                self.path_slug[data_point.raw_response.path.slug]
-            ] += sum([i.get("co2e", 0) for i in data_point.json_holder])
-            self.top_emission_by_location[data_point.locale.name] += sum(
-                [i.get("co2e", 0) for i in data_point.json_holder]
-            )
-            for emission_request, climatiq_response in zip(
-                data_point.raw_response.data, data_point.json_holder
-            ):
-                self.top_emission_by_source[
-                    emission_request["Emission"]["Category"]
-                ] += climatiq_response.get("co2e", 0)
-
-        return self.calculate_scope_contribution(
-            key_name="scope", scope_total_values=top_emission_by_scope
-        )
-
     def get(self, request, format=None):
         """
-        Returns a dictionary with keys containing
+        Returns a dictionary with keys containing:
         1. Top Emission by Scope
         2. Top Emission by Source
         3. Top Emission by Location
-        filtered by Organisation, Corporate and Year"""
+        filtered by Organisation, Corporate, and Year.
+        """
 
         # * Get all the RawResponses
         serializer = CheckAnalysisViewSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
-        self.start = serializer.validated_data["start"]
-        self.end = serializer.validated_data["end"]
-        self.corporate = serializer.validated_data.get(
-            "corporate"
-        )  # * This is optional
-        self.organisation = serializer.validated_data["organisation"]
-        self.location = serializer.validated_data.get("location")  # * This is optional
+        start = serializer.validated_data["start"]
+        end = serializer.validated_data["end"]
+        corporate = serializer.validated_data.get("corporate")  # * This is optional
+        organisation = serializer.validated_data["organisation"]
+        location = serializer.validated_data.get("location")  # * This is optional
+
         # * Set Locations Queryset
-        self.locations = set_locations_data(
-            organisation=self.organisation,
-            corporate=self.corporate,
-            location=self.location,
+        locations = set_locations_data(
+            organisation=organisation,
+            corporate=corporate,
+            location=location,
         )
+
         # * Get top emissions by Scope
+        top_emission_by_scope, top_emission_by_source, top_emission_by_location = (
+            get_top_emission_by_scope(
+                locations=locations,
+                request=request,
+                start=start,
+                end=end,
+                path_slug=self.path_slug,
+            )
+        )
+
+        # * Prepare response data
         response_data = dict()
-        response_data["all_emission_by_scope"] = self.get_top_emission_by_scope()
-        response_data["all_emission_by_source"] = self.calculate_scope_contribution(
-            key_name="source", scope_total_values=self.top_emission_by_source
+        response_data["all_emission_by_scope"] = calculate_scope_contribution(
+            key_name="scope", scope_total_values=top_emission_by_scope
         )
-        response_data["all_emission_by_location"] = self.calculate_scope_contribution(
-            key_name="location", scope_total_values=self.top_emission_by_location
+        response_data["all_emission_by_source"] = calculate_scope_contribution(
+            key_name="source", scope_total_values=top_emission_by_source
         )
-        response_data["top_5_emisson_by_source"] = response_data["all_emission_by_source"][0:5]
-        response_data["top_5_emisson_by_location"] = response_data["all_emission_by_location"][0:5]
-        response_data["selected_org"] = self.organisation.name
-        response_data["selected_corporate"] = self.corporate.name if self.corporate else None
-        response_data["selected_location"] = self.location.name if self.location else None
-        response_data["selected_start_date"] = self.start
-        response_data["selected_end_date"] = self.end
+        response_data["all_emission_by_location"] = calculate_scope_contribution(
+            key_name="location", scope_total_values=top_emission_by_location
+        )
+        response_data["top_5_emisson_by_source"] = response_data[
+            "all_emission_by_source"
+        ][0:5]
+        response_data["top_5_emisson_by_location"] = response_data[
+            "all_emission_by_location"
+        ][0:5]
+        response_data["selected_org"] = organisation.name
+        response_data["selected_corporate"] = corporate.name if corporate else None
+        response_data["selected_location"] = location.name if location else None
+        response_data["selected_start_date"] = start
+        response_data["selected_end_date"] = end
+
         return Response({"data": response_data}, status=status.HTTP_200_OK)
