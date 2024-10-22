a21f7d91a78877a8f31e4c12f5b8a985a0b69ed4
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-22 11:50:42 +0530

    Removed validations from screen fifteen

--- esg_report/models/ScreenFifteen.py
+++ esg_report/models/ScreenFifteen.py
@@ -15,20 +15,28 @@ class ScreenFifteenModel(AbstractModel, HistoricalModelMixin):
     commitment_statement = models.TextField(
         verbose_name="Company's Commitment to Products and Services",
         help_text="Add a statement about the company's commitment to products and services.",
+        null=True,
+        blank=True,
     )
 
     product_info_labelling = models.TextField(
         verbose_name="Product and Service Information and Labelling",
         help_text="Add a statement about the company's product and service information and labelling.",
+        null=True,
+        blank=True,
     )
 
     marketing_practices = models.TextField(
         verbose_name="Company's Marketing Practices",
         help_text="Add a statement about the company's marketing practices.",
+        null=True,
+        blank=True,
     )
-
     conclusion = models.TextField(
-        verbose_name="Conclusion", help_text="Add a conclusion to the report."
+        verbose_name="Conclusion",
+        help_text="Add a conclusion to the report.",
+        null=True,
+        blank=True,
     )
 
     def __str__(self):
