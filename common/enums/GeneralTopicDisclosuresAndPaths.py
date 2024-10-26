c0971858df10d167fa57f672077c1d4543c72235
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-10-26 14:10:50 +0530

    Added Indicator Numbers with Disclosure

--- common/enums/GeneralTopicDisclosuresAndPaths.py
+++ common/enums/GeneralTopicDisclosuresAndPaths.py
@@ -1,76 +1,110 @@
 GENERAL_DISCLOSURES_AND_PATHS = {
     "Org Details": [
-        ("Organizational Details", "gri-general-org_details_2-1a-1b-1c-1d")
+        [("Organizational Details", "gri-general-org_details_2-1a-1b-1c-1d")],
+        "2-1",
     ],
     "Entities": [
-        ("List of entities", "gri-general-entities-list_of_entities-2-2-a"),
-        (
-            "Audited, consolidated financial statements",
-            "gri-general-entities-audited-2-2-b",
-        ),
-        ("Multiple entities", "gri-general-entities-multiple-2-2-c"),
+        [
+            ("List of entities", "gri-general-entities-list_of_entities-2-2-a"),
+            (
+                "Audited, consolidated financial statements",
+                "gri-general-entities-audited-2-2-b",
+            ),
+            ("Multiple entities", "gri-general-entities-multiple-2-2-c"),
+        ],
+        "2-2",
     ],
     "Report Details": [
-        (
-            "Reporting period and frequency",
-            "gri-general-report_details-reporting_period-2-3-a",
-        ),
-        (
-            "Reporting period for financial reporting",
-            "gri-general-report_details-reporting_period-2-3-b",
-        ),
-        ("Publication date", "gri-general-report_details-publication-2-3-c"),
-        ("Point of contact", "gri-general-report_details-point-2-3-d"),
+        [
+            (
+                "Reporting period and frequency",
+                "gri-general-report_details-reporting_period-2-3-a",
+            ),
+            (
+                "Reporting period for financial reporting",
+                "gri-general-report_details-reporting_period-2-3-b",
+            ),
+            ("Publication date", "gri-general-report_details-publication-2-3-c"),
+            ("Point of contact", "gri-general-report_details-point-2-3-d"),
+        ],
+        "2-3",
     ],
-    "Restatement": [("Restatements of Information", "gri-general-restatements-2-4-a")],
+    "Restatement": [
+        [("Restatements of Information", "gri-general-restatements-2-4-a")],
+        "2-4",
+    ],
     "Assurance": [
-        (
-            "Policy and practice: External assurance",
-            "gri-general-assurance-policy-2-5-a",
-        ),
-        (
-            "Highest governance body and senior executive",
-            "gri-general-assurance-highest-2-5-a",
-        ),
-        (
-            "External assurance of sustainability report",
-            "gri-general-assurance-external-2-5-b",
-        ),
+        [
+            (
+                "Policy and practice: External assurance",
+                "gri-general-assurance-policy-2-5-a",
+            ),
+            (
+                "Highest governance body and senior executive",
+                "gri-general-assurance-highest-2-5-a",
+            ),
+            (
+                "External assurance of sustainability report",
+                "gri-general-assurance-external-2-5-b",
+            ),
+        ],
+        "2-5",
     ],
     "Business Details": [
-        (
-            "Organisation's sector/sectors",
-            "gri-general-business_details-organisation-2-6a",
-        ),
-        ("Value Chain", "gri-general-business_details-value-2-6b"),
-        (
-            "Other relevant business relationships",
-            "gri-general-business_details-other-2-6c",
-        ),
-        ("Changes in the information", "gri-general-business_details-changes-2-6d"),
+        [
+            (
+                "Organisation's sector/sectors",
+                "gri-general-business_details-organisation-2-6a",
+            ),
+            ("Value Chain", "gri-general-business_details-value-2-6b"),
+            (
+                "Other relevant business relationships",
+                "gri-general-business_details-other-2-6c",
+            ),
+            ("Changes in the information", "gri-general-business_details-changes-2-6d"),
+        ],
+        "2-6",
     ],
     "Workforce-Employees": [
-        "gri-general-workforce_employees-2-7-a-b-permanent_employee",
-        "gri-general-workforce_employees-methodologies-2-7c",
-        "gri-general-workforce_employees-data-2-7c",
-        "gri-general-workforce_employees-contextual-2-7d",
-        "gri-general-workforce_employees-fluctuations-2-7e",
+        [
+            "gri-general-workforce_employees-2-7-a-b-permanent_employee",
+            "gri-general-workforce_employees-methodologies-2-7c",
+            "gri-general-workforce_employees-data-2-7c",
+            "gri-general-workforce_employees-contextual-2-7d",
+            "gri-general-workforce_employees-fluctuations-2-7e",
+        ],
+        "2-7",
     ],
     "Workforce-Other Workers": [
-        "gri-general-workforce_other_workers-workers-2-8-a",
-        "gri-general-workforce_other_workers-methodologies-2-8b",
-        "gri-general-workforce_other_workers-fluctuations-2-8c",
+        [
+            "gri-general-workforce_other_workers-workers-2-8-a",
+            "gri-general-workforce_other_workers-methodologies-2-8b",
+            "gri-general-workforce_other_workers-fluctuations-2-8c",
+        ],
+        "2-8",
     ],
     "Laws and Regulation": [
-        "gri-general-laws_and_regulation-instance-2-27-a",
-        "gri-general-laws_and_regulation-monetary-2-27-b",
-        "gri-general-laws_and_regulation-significant-2-27-c",
-        "gri-general-laws_and_regulation-organization-2-27-d",
+        [
+            "gri-general-laws_and_regulation-instance-2-27-a",
+            "gri-general-laws_and_regulation-monetary-2-27-b",
+            "gri-general-laws_and_regulation-significant-2-27-c",
+            "gri-general-laws_and_regulation-organization-2-27-d",
+        ],
+        "2-27",
     ],
-    "Membership & Association": ["gri-general-membership_association-2-28-a-report"],
-    "Stakeholder Engagement": ["gri-general-stakeholder_engagement-2-29a-describe"],
+    "Membership & Association": [
+        ["gri-general-membership_association-2-28-a-report"],
+        "2-28",
+    ],
+    "Stakeholder Engagement": [
+        ["gri-general-stakeholder_engagement-2-29a-describe"],
+        "2-29",
+    ],
     "Collective Bargaining Agreements": [
-        "gri-general-collective_bargaining-2-30-a-percentage",
-        "gri-general-collective_bargaining-2-30-b-employees",
+        [
+            "gri-general-collective_bargaining-2-30-a-percentage",
+            "gri-general-collective_bargaining-2-30-b-employees",
+        ],
+        "2-30",
     ],
 }
