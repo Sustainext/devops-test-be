d86c65ca48a24dc6f8e1ddecba2ae573aa378d63
Utsav <utsav.pipersaniya@sustainext.ai>
2024-10-26 16:04:31 +0530

    preference updated

--- sustainapp/Views/PreferencesView.py
+++ sustainapp/Views/PreferencesView.py
@@ -187,7 +187,7 @@ def OrgPreference(request):
             return JsonResponse(
                 {"error": "Organization not found"}, status=status.HTTP_404_NOT_FOUND
             )
-        org_data = Organization.objects.filter(id=org.id).first()
+        org_data = org.first()
         if org_data:
             # Fetching related data for organization
             sdgs = []
