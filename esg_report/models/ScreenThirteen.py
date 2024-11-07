033de62e2a72f00f98fd68fcc5460679bf2afc30
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-11-07 17:48:48 +0530

    Convert JSONField to TextField in ESG Screens

--- esg_report/models/ScreenThirteen.py
+++ esg_report/models/ScreenThirteen.py
@@ -12,84 +12,84 @@ class ScreenThirteen(AbstractModel, HistoricalModelMixin):
     report = models.OneToOneField(
         Report, on_delete=models.CASCADE, related_name="screen_thirteen"
     )
-    employee_policies_statement = models.JSONField(
+    employee_policies_statement = models.TextField(
         verbose_name="Employee Policies Statement",
         help_text="Statement about company's employees and their policies",
         blank=True,
         null=True,
     )
 
-    workforce_hire_retention_statement = models.JSONField(
+    workforce_hire_retention_statement = models.TextField(
         verbose_name="Workforce Hire and Retention Statement",
         help_text="Statement about company's workforce hire and retention strategies",
         blank=True,
         null=True,
     )
 
-    standard_wage = models.JSONField(
+    standard_wage = models.TextField(
         verbose_name="Standard Wage",
         help_text="Information about the company's standard wage policies",
         blank=True,
         null=True,
     )
 
-    performance_review_process = models.JSONField(
+    performance_review_process = models.TextField(
         verbose_name="Performance Review Process",
         help_text="Statement about company's process for performance review of employees",
         blank=True,
         null=True,
     )
 
-    forced_labor_position = models.JSONField(
+    forced_labor_position = models.TextField(
         verbose_name="Position on Forced/Compulsory Labor",
         help_text="Statement about company's position on forced or compulsory labor",
         blank=True,
         null=True,
     )
 
-    child_labor_position = models.JSONField(
+    child_labor_position = models.TextField(
         verbose_name="Position on Child Labor",
         help_text="Statement about company's position on child labor",
         blank=True,
         null=True,
     )
 
-    employee_diversity_position = models.JSONField(
+    employee_diversity_position = models.TextField(
         verbose_name="Position on Employee Diversity",
         help_text="Statement about company's position on diversity of employees",
         blank=True,
         null=True,
     )
 
-    employee_skill_upgrade_programs = models.JSONField(
+    employee_skill_upgrade_programs = models.TextField(
         verbose_name="Employee Skill Upgrade Programs",
         help_text="Statement about company's programs for upgrading employee's skills",
         blank=True,
         null=True,
     )
 
-    remuneration_practices = models.JSONField(
+    remuneration_practices = models.TextField(
         verbose_name="Remuneration Practices and Policies",
         help_text="Statement about company's remuneration practices and policies",
         blank=True,
         null=True,
     )
 
-    ohs_policies = models.JSONField(
+    ohs_policies = models.TextField(
         verbose_name="Occupational Health and Safety Policies",
         help_text="Statement about company's Occupational Health and Safety (OHS) policies",
         blank=True,
         null=True,
     )
 
-    hazard_risk_assessment = models.JSONField(
+    hazard_risk_assessment = models.TextField(
         verbose_name="Hazard and Risk Assessment Process",
         help_text="Statement about company's process of Hazard and risk assessment",
         blank=True,
         null=True,
     )
 
-    work_related_health_injuries = models.JSONField(
+    work_related_health_injuries = models.TextField(
         verbose_name="Work-Related Ill Health and Injuries",
         help_text="Statement about work-related ill health and injuries in the company",
         blank=True,
@@ -97,54 +97,54 @@ class ScreenThirteen(AbstractModel, HistoricalModelMixin):
     )
 
     # New fields
-    safety_training = models.JSONField(
+    safety_training = models.TextField(
         verbose_name="Safety Training",
         help_text="Statement about the company's safety training programs for employees",
         blank=True,
         null=True,
     )
 
-    ohs_management_system = models.JSONField(
+    ohs_management_system = models.TextField(
         verbose_name="OHS Management System",
         help_text="Statement about the company's Occupational Health and Safety (OHS) management system",
         blank=True,
         null=True,
     )
 
-    freedom_of_association_views = models.JSONField(
+    freedom_of_association_views = models.TextField(
         verbose_name="Views on Freedom of Association and Collective Bargaining",
         help_text="Statement about the company's views on freedom of association and collective bargaining",
         blank=True,
         null=True,
     )
 
-    violation_discrimination_policy = models.JSONField(
+    violation_discrimination_policy = models.TextField(
         verbose_name="Policy on Addressing Violation/Discrimination",
         help_text="Statement about the company's policy for addressing violation or discrimination",
         blank=True,
         null=True,
     )
 
-    indigenous_rights_policy = models.JSONField(
+    indigenous_rights_policy = models.TextField(
         verbose_name="Policy on Violation of Indigenous People's Rights",
         help_text="Statement about the company's policy on violation of the rights of indigenous people",
         blank=True,
         null=True,
     )
 
-    parental_leaves = models.JSONField(
+    parental_leaves = models.TextField(
         verbose_name="Dynamic Question Answer",
         help_text="This field stores the answer to a dynamic question provided as input",
         blank=True,
         null=True,
     )
-    security_personnel_internal_training = models.JSONField(
+    security_personnel_internal_training = models.TextField(
         verbose_name="Percentage of Security Personnel Who Have Received Formal Training in the Organisation",
         help_text="Percentage of security personnel who have received formal training in the organisation",
         blank=True,
         null=True,
     )
-    security_personnel_external_training = models.JSONField(
+    security_personnel_external_training = models.TextField(
         verbose_name="Percentage of Security Personnel Who Have Received Formal Training from Third-Party Organisation",
         help_text="Percentage of security personnel who have received formal training from third-party organisation",
         blank=True,
--- esg_report/models/ScreenTwelve.py
+++ esg_report/models/ScreenTwelve.py
@@ -12,110 +12,110 @@ class ScreenTwelve(AbstractModel, HistoricalModelMixin):
     report = models.OneToOneField(
         Report, on_delete=models.CASCADE, related_name="screen_twelve"
     )
-    environmental_responsibility_statement = models.JSONField(
+    environmental_responsibility_statement = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's responsibility to minimize the environmental impact",
     )
-    emissions = models.JSONField(
+    emissions = models.TextField(
         blank=True,
         null=True,
         help_text="Description of the company's efforts to reduce emissions",
     )
-    scope_one_emissions = models.JSONField(
+    scope_one_emissions = models.TextField(
         blank=True,
         null=True,
         help_text="Add statement about company’s scope 1 emissions",
     )
-    scope_two_emissions = models.JSONField(
+    scope_two_emissions = models.TextField(
         blank=True,
         null=True,
         help_text="Add statement about company’s scope 2 emissions",
     )
-    scope_three_emissions = models.JSONField(
+    scope_three_emissions = models.TextField(
         blank=True,
         null=True,
         help_text="Add statement about company’s scope 3 emissions",
     )
-    ghg_emission_intensity_tracking = models.JSONField(
+    ghg_emission_intensity_tracking = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about tracking of GHG emission intensity",
     )
-    ghg_emission_reduction_efforts = models.JSONField(
+    ghg_emission_reduction_efforts = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about efforts to reduce GHG emissions",
     )
-    ozone_depleting_substance_elimination = models.JSONField(
+    ozone_depleting_substance_elimination = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's commitment to eliminate use of ozone depleting substances",
     )
-    material_management_strategy = models.JSONField(
+    material_management_strategy = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's material management strategy",
     )
-    recycling_process = models.JSONField(
+    recycling_process = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's process for recycling",
     )
-    reclamation_recycling_process = models.JSONField(
+    reclamation_recycling_process = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's reclamation and recycling process",
     )
-    water_withdrawal_tracking = models.JSONField(
+    water_withdrawal_tracking = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's tracking of water withdrawal",
     )
-    water_consumption_goals = models.JSONField(
+    water_consumption_goals = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's water consumption goals",
     )
-    energy_consumption_within_organization = models.JSONField(
+    energy_consumption_within_organization = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's energy consumption within the organization",
     )
-    energy_consumption_outside_organization = models.JSONField(
+    energy_consumption_outside_organization = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's energy consumption outside of the organization",
     )
-    energy_intensity_tracking = models.JSONField(
+    energy_intensity_tracking = models.TextField(
         blank=True, null=True, help_text="Statement about tracking the Energy Intensity"
     )
-    energy_consumption_reduction_commitment = models.JSONField(
+    energy_consumption_reduction_commitment = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's commitment to reduce energy consumption",
     )
-    significant_spills = models.JSONField(
+    significant_spills = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's significant spills",
     )
-    habitat_protection_restoration_commitment = models.JSONField(
+    habitat_protection_restoration_commitment = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's commitment to protect and restore habitats",
     )
-    air_quality_protection_commitment = models.JSONField(
+    air_quality_protection_commitment = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's commitment to protect and maintain air quality",
     )
-    biogenic_c02_emissions = models.JSONField(
+    biogenic_c02_emissions = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's biogenic CO2 emissions",
     )
-    biogenic_c02_emissions_305_3c = models.JSONField(
+    biogenic_c02_emissions_305_3c = models.TextField(
         blank=True,
         null=True,
         help_text="Statement about company's biogenic CO2 emissions 305 3c",
