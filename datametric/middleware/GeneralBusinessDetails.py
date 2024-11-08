637c92212e4dea7e6f893c47bc64340f8b766f5e
Yashwanth39 <yashwanth.vanimina@sustainext.ai>
2024-11-08 09:26:51 +0530

    Changed the encryption algo and the added the public in middleware

--- datametric/middleware/GeneralBusinessDetails.py
+++ datametric/middleware/GeneralBusinessDetails.py
@@ -35,20 +35,22 @@ class PathSlugMiddleware(MiddlewareMixin):
                     'Sub_industry': corporate_entity.subindustry
                 })
             return sector_subindustry_data
-        
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         if request.method == 'GET' and request.path == '/datametric/get-fieldgroups':
             auth_header = request.headers.get("Authorization")
             query_params = parse_qs(urlparse(request.get_full_path()).query)
             path_name = query_params.get('path_slug', [None])[0]
-            
+
             if path_name == 'gri-general-business_details-organisation-2-6a':
                 if auth_header:
                     token = auth_header.split(" ")[1]
+                    with open("public_key.pem", "rb") as key_file:
+                        public_key = key_file.read()
                     payload = jwt.decode(
                         token,
-                        settings.SECRET_KEY,
-                        algorithms=["HS256"],
+                        public_key,
+                        algorithms=["RS256"],
                     )
                     client_head = payload.get("client_id")
                     user_head = payload.get("user_id")
@@ -59,7 +61,7 @@ class PathSlugMiddleware(MiddlewareMixin):
                 user = user_head
                 client = client_head
                 data = self.get_sector_data(request, organization, corporate)
-                
+
                 # Fetch the Path instance
                 path_instance = Path.objects.get(slug=path_name)
                 if corporate =='':
