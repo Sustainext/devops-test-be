da4c9856c7137952227b6838eff3b5b3fc2fa426
Yashwanth39 <yashwanth.vanimina@sustainext.ai>
2024-11-29 10:36:53 +0530

    Updating the necessary fields on Organization

--- datametric/admin.py
+++ datametric/admin.py
@@ -70,7 +70,8 @@ class DataPointAdmin(admin.ModelAdmin):
         "year",
         "month",
     )
-    search_fields = ["path__slug"]
+    search_fields = ["path__slug", "metric_name", "raw_response__id", "index"]
+    list_filter = ("index",)
 
 
 admin.site.register(DataPoint, DataPointAdmin)
