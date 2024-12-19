512079568b91c7785c0787a144b8971e2f0b15f6
JairajSustained <jairaj.sahgal@sustainext.ai>
2024-12-19 17:00:15 +0530

    Added exception handler

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "EXCEPTION_HANDLER": "sustainapp.utils.custom_exception_handler",
+    "EXCEPTION_HANDLER": "common.utils.global_exception_handler.custom_exception_handler",
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
