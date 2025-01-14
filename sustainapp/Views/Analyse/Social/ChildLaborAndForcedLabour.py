010d7cbf5101a7925c3b01ab595c8253e57f9267
JairajSustained <jairaj.sahgal@sustainext.ai>
2025-01-14 16:54:51 +0530

    Merged analysis and removed some endpoints

--- sustainapp/Views/Analyse/Social/ChildLaborAndForcedLabour.py
+++ sustainapp/Views/Analyse/Social/ChildLaborAndForcedLabour.py
@@ -1,5 +1,5 @@
-from datametric.models import DataPoint, RawResponse, Path, DataMetric
-from sustainapp.models import Organization, Corporateentity, Location
+from datametric.models import DataPoint
+from sustainapp.models import Corporateentity
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,15 +7,16 @@ from rest_framework.permissions import IsAuthenticated
 from sustainapp.Serializers.CheckAnalysisViewSerializer import (
     CheckAnalysisViewSerializer,
 )
-from rest_framework import serializers
-from datametric.utils.analyse import set_locations_data
-from datametric.utils.analyse import filter_by_start_end_dates
+from collections import defaultdict
 from logging import getLogger
 
 logger = getLogger("error.log")
 
 
-class ChildLabourAnalyzeView(APIView):
+class ChildLabourAndForcedLabourAnalyzeView(APIView):
+    """
+    This view is used to get the child labour analysis and forced labour analysis.
+    """
 
     permission_classes = [IsAuthenticated]
 
@@ -100,6 +101,80 @@ class ChildLabourAnalyzeView(APIView):
 
         return self.call_commoncode(path, child_labour_dp)
 
+    def call_common_code(self, path_slug, data_points):
+        # Define the categories we are interested in
+        if path_slug == "gri-social-human_rights-409-1a-suppliers_forced_labor":
+            metric_categories = [
+                "TypeofOperation",
+                "geographicareas",
+                "compulsorylabor",
+            ]
+        else:
+            metric_categories = ["TypeofOperation", "geographicareas", "childlabor"]
+
+        forced_labor_data = defaultdict(lambda: {})
+
+        for data in data_points:
+            if data.metric_name in metric_categories:
+                key = (
+                    data.index,
+                    data.year,
+                    data.month,
+                )
+                forced_labor_data[key][data.metric_name] = data.value
+        # print(forced_labor_data)
+
+        unique_entries = {}
+        for entry in forced_labor_data.values():
+            identifier = (entry.get("TypeofOperation"), entry.get("geographicareas"))
+            if identifier not in unique_entries:
+                unique_entries[identifier] = entry
+
+        results = list(unique_entries.values())
+
+        return results
+
+    def get_forced_labor_data(
+        self,
+        organisation,
+        corporate,
+        year,
+        path_slug,
+        client_id,
+    ):
+
+        data_points = DataPoint.objects.filter(
+            organization=organisation,
+            corporate=corporate,
+            path__slug=path_slug,
+            client_id=client_id,
+            year=year,
+        )
+
+        if not data_points:
+
+            logger.error(
+                f"No data found for Forced labour analysis for organisation {organisation}, corporate {corporate}, path {path_slug}, client id {client_id} and year {year}"
+            )
+
+            if not corporate:
+                corps_of_org = Corporateentity.objects.filter(organization=organisation)
+                res = []
+                # filtering the data as per corporate
+                for a_corp in corps_of_org:
+                    forced_labour_corp_dp = DataPoint.objects.filter(
+                        organization=organisation,
+                        corporate=a_corp,
+                        path__slug=path_slug,
+                        client_id=client_id,
+                        year=year,
+                    )
+                    res.extend(self.call_common_code(path_slug, forced_labour_corp_dp))
+                return res
+            return []
+
+        return self.call_common_code(path_slug, data_points)
+
     def get(self, request):
         serializer = CheckAnalysisViewSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
@@ -109,7 +184,6 @@ class ChildLabourAnalyzeView(APIView):
 
             self.organisation = serializer.validated_data["organisation"]
             self.corporate = serializer.validated_data.get("corporate", None)
-            # self.location = serializer.validated_data.get("location", None)
 
             self.clients_id = request.user.client.id
 
@@ -118,11 +192,6 @@ class ChildLabourAnalyzeView(APIView):
                     "Start and End dates must be in the same year",
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-            # self.locations = set_locations_data(
-            #     organisation=self.organisation,
-            #     corporate=self.corporate,
-            #     location=self.location,
-            # )
 
             operations_childlabor = self.process_childlabor(
                 "gri-social-human_rights-408-1a-1b-operations_risk_child_labour"
@@ -137,8 +206,25 @@ class ChildLabourAnalyzeView(APIView):
                 "gri-social-human_rights-408-1a-1b-supplier_young_worker_exposed"
             )
 
+            forced_labor_operations = self.get_forced_labor_data(
+                self.organisation,
+                self.corporate,
+                self.from_date.year,
+                "gri-social-human_rights-409-1a-operations_forced_labor",
+                request.user.client_id,
+            )
+            forced_labor_suppliers = self.get_forced_labor_data(
+                self.organisation,
+                self.corporate,
+                self.from_date.year,
+                "gri-social-human_rights-409-1a-suppliers_forced_labor",
+                request.user.client_id,
+            )
+
             return Response(
                 {
+                    "operations_considered_to_have_significant_risk_for_incidents_of_forced_or_compulsary_labor": forced_labor_operations,
+                    "suppliers_at_significant_risk_for_incidents_of_forced_or_compulsory_labor": forced_labor_suppliers,
                     "operation_significant_risk_of_child_labor": operations_childlabor,
                     "operation_significant_risk_of_young_workers": operations_young_workers,
                     "suppliers_significant_risk_of_child_labor": suppliers_childlabor,
