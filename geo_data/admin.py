9ae9322e7bac660d6a317b2a682350a022992f30
naveensustain <naveen.kumar@sustainext.ai>
2025-06-06 13:51:32 +0530

    Geo data changes

--- geo_data/admin.py
+++ geo_data/admin.py
@@ -2,6 +2,22 @@
 from django.contrib import admin
 from .models import Country, State, City
 
-admin.site.register(Country)
-admin.site.register(State)
-admin.site.register(City)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['country_name', 'sortname', 'slug']
+    list_filter = ['sortname']
+    list_display = ['country_name', 'sortname', 'slug']
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['state_name', 'country']
+    list_filter = ['country']
+    search_fields = ['state_name']
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['city_name', 'state']
+    list_filter = ['state']
+    search_fields = ['city_name']
+
