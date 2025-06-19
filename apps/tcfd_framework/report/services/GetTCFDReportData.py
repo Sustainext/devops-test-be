125fbb315e000a781bc0e579e6066ee159a7a3a5
JairajSustained <jairaj.sahgal@sustainext.ai>
2025-06-19 14:08:32 +0530

    Added mapping of slugs with their screens.

--- apps/tcfd_framework/report/services/GetTCFDReportData.py
+++ apps/tcfd_framework/report/services/GetTCFDReportData.py
@@ -28,11 +28,26 @@ class GetTCFDReportData:
                 "physical_risks": "gri-economic-climate_related_risks-202-2a-physical_risk",
                 "transition_risk": "gri-economic-climate_related_risks-202-2a-transition_risk",
                 "other_risks": "gri-economic-climate_related_risks-202-2a-other_risk",
+                "general_business_impact": "impact-of-climate-related-issues-on-business",
+                "ghg_emissions_reduction_commitments": "gri-governance-management_of_impact-2-12-b-due_diligence",
+                "strategy_resilience_to_climate_related_risks_and_opportunities": "economic-climate-risks-and-opportunities-resilience-of-the-organisations-strategy",
             },
-            "risk_management": [],
-            "metrics_targets": [],
+            "risk_management": {
+                "climate_related_risk_identification_assessment_process": "governance-risk-management-risk-identification-assessment-screen1",
+                "risk_significance": "governance-risk-management-risk-identification-assessment-screen2",
+                "regulatory_considerations": "governance-risk-management-risk-identification-assessment-screen3",
+                "size_scope_of_risk": "governance-risk-management-risk-identification-assessment-screen4",
+                "climate_risks_integration_into_organisations_risk_management_framework": "governance-risk-management-climate-risk-integration",
+                "climate_risk_management_process": "governance-risk-management-climate-risk-management-screen1",
+                "materiality_determination": "governance-risk-management-climate-risk-management-screen2",
+            },
+            "metrics_targets": {
+                "metrics_used_to_assess_climate_related_risks_and_opportunities": "economic-climate-risks-and-opportunities-climate-related-metrics",
+                "metrics_used_to_assess_climate_related_opportunities": "economic-climate-risks-and-opportunities-climate-related-metrics-screen2",
+                "targets_used_to_manage_climate_related_risks_and_opportunities_and_performance_against_targets": "economic-climate-risks-and-opportunities-climate-related-targets",
+            },
             "tcfd_content_index": [],
             "annexure": [],
         }
 
-    def get_data_as_per_screen(self, screen_name: str): ...
+    def get_data_as_per_screen(self, screen_name: str):...
